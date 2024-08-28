412ca5afcef49fcdbfddf9b634f88ec94d07ff2a
Junting Lu (FESCO Adecco Human Resources) <v-juntinglu@microsoft.com>
2024-08-28 20:39:29 +0800

    add print task id

--- app/app.py
+++ app/app.py
@@ -6,11 +6,20 @@ from flask import Flask, jsonify,request
 import os,json
 from ufo.module.client import UFOClientManager
 from ufo.module.sessions.session import SessionFactory
+import win32com.client
 
 app = Flask(__name__)
 
 tasks_dir = "sample/word/tasks/"
 logs_dirs = "logs/"
+
+def close_docs():
+    """
+    Close all opened word documents
+    """
+    client = win32com.client.Dispatch("Word.Application")
+    if client.Documents.Count:
+        os.system("taskkill /f /im WINWORD.EXE")
 
 def extract_thoughts(task_log:str)->list:
     """
@@ -40,29 +49,24 @@ def consume():
     user_name = data.get('user_name')
     task_file = data.get('task_file')
     mode="batch_normal"
+    close_docs()
     task_log = user_name + "_" + datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    print(f"Start to execute Task {os.path.basename(task_file).split('.')[0]}")
     sessions = SessionFactory().create_session(
         task=task_log, mode=mode, plan=tasks_dir + task_file
     )
     clients = UFOClientManager(sessions)
     res = clients.run_all()
     # extract thoughts from the response.log
-    if res:
-        thoughts = extract_thoughts(task_log)
-        # return thoughts to the client
-        return jsonify({
-            "status": "Finish UFO",
-            "user_name": user_name,
-            "task_file":task_file,
-            "thoughts": thoughts
-        })
-    else:
-        return jsonify({
-            "status": "UFO Time out",
-            "user_name": user_name,
-            "task_file":task_file,
-            "thoughts": "TIME OUT"
-        })
+    thoughts = extract_thoughts(task_log)
+    # return thoughts to the client
+    return jsonify({
+        "status": "Finish UFO",
+        "user_name": user_name,
+        "task_file":task_file,
+        "thoughts": thoughts
+    })
+    
 
 def main():
     app.run(host='localhost', port=6000)
